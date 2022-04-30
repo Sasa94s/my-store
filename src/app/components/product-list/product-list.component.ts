37ed364745cc1b5b036a686c579aecd9685329c8
Mostafa ElSheikh <mostafa.elsheikh@vodafone.com>
2022-04-30 17:23:32 +0200

    feat: scaffold angular components

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['../../product-list/product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
