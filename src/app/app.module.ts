3481237a26d60e52728813bd85ffa055e0a4381d
Mostafa ElSheikh <mostafa.elsheikh@vodafone.com>
2022-05-01 02:35:07 +0200

    chore: reformat app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ConfirmationComponent } from './components/confirmation/confirmation.component';
-import { ProductItemComponent } from './components/product-item/product-item.component';
-import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { HeaderComponent } from './layout/header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CartComponent} from './components/cart/cart.component';
+import {ConfirmationComponent} from './components/confirmation/confirmation.component';
+import {ProductItemComponent} from './components/product-item/product-item.component';
+import {ProductItemDetailComponent} from './components/product-item-detail/product-item-detail.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {HeaderComponent} from './layout/header/header.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FlexModule} from "@angular/flex-layout";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatOptionModule} from "@angular/material/core";
 import {MatSelectModule} from "@angular/material/select";
@@ -41,4 +41,5 @@ import {MatCardModule} from "@angular/material/card";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
