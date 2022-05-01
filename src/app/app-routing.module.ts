e30fb2a1fac5d2fb53c3615011b9265d332e716d
Mostafa ElSheikh <mostafa.elsheikh@vodafone.com>
2022-05-01 05:47:31 +0200

    refactor: add route for single product view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from "./components/product-list/product-list.component";
+import {ProductItemComponent} from "./components/product-item/product-item.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', component: ProductListComponent, data: { title: 'Products List' } },
-  { path: '**', redirectTo: 'products' },
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', component: ProductListComponent, data: {title: 'Products List'}},
+  {path: 'product/:id', component: ProductItemComponent, data: {title: 'Product'}},
+  {path: '**', redirectTo: 'products'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ProductService} from "../../shared/services/product.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-item',
@@ -8,14 +10,25 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ProductItemComponent implements OnInit {
 
   @Input() product: Product | undefined;
+  @Input() productId: number | undefined;
   addToCartOptions: number[];
 
-  constructor() {
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
     // initialize an array with 1 to 10 values
     this.addToCartOptions = [...Array(11).keys()].slice(1);
   }
 
   ngOnInit(): void {
+    if (!this.productId) {
+      this.productId = Number(this.route.snapshot.paramMap.get('productId'));
+    }
+    if (!this.product) {
+      this.productService.getProduct(this.productId || 1)
+        .subscribe(products => products.length !== 0 ? this.product = products[0] : undefined);
+    }
   }
 
   getPrice(): string {
